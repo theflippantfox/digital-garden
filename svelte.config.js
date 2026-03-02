d3f87b70938dc75197e32d1a723855cea8349c72
TheFlippantFox <theflippantfox@gmail.com>
2026-03-02 13:23:19 +0530

    update: dynamic adapters

--- svelte.config.js
+++ svelte.config.js
@@ -2,28 +2,24 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 const platform = process.env.PLATFORM;
-let adapter
-
-if (platform === 'cloudflare') {
-  adapter = adapterCloudflare();
-} else {
-  adapter = adapterVercel();
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      // Routes that can't be server-rendered (D3 canvas, pure interactivity)
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-    })
+    adapter:
+      platform === 'cloudflare'
+        ? adapterCloudflare({
+          routes: {
+            include: ['/*'],
+            exclude: ['<all>']
+          }
+        })
+        : adapterVercel({
+        })
   }
 };
 
 export default config;
+
