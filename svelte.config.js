73b2bb0efd07a873bdfce59dec12f438d9935ec8
TheFlippantFox <theflippantfox@gmail.com>
2026-03-02 13:20:46 +0530

    update: dynamic adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
+const platform = process.env.PLATFORM;
+let adapter
+
+if (platform === 'cloudflare') {
+  adapter = adapterCloudflare();
+} else {
+  adapter = adapterVercel();
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
